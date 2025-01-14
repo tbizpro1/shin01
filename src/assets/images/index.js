17a97e33069d78eecb5cffea45dc9e71fe8f1492
lucasleytao <lucassilvaleitao1401@gmail.com>
2025-01-14 11:07:09 -0300

    build lucas

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,6 +3,7 @@ import backgroundLogin from './background-login.jpg';
 import logoShin from './stp1.png';
 import logoLoader from './logo.svg';
 import profileUser from './profile_av.jpg'
+import perfilUnd from './perfilnd.png'
 import socio from './socio.png'
 // Importe outras imagens aqui...
 
@@ -12,5 +13,6 @@ export {
   logoLoader,
   profileUser,
   socio,
+  perfilUnd,
   // Exporte outras imagens...
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { AuthProvider } from './app/context/authContext.jsx'
-import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from 'react' //ajuda a identificar possiveis problemas na aplicacao
+import { createRoot } from 'react-dom/client' //cria a raiz de um componente react dentro de um elemento html
+import App from './App.jsx' //importa o componente principal que e o ponto de entrada da aplicacao
+import { AuthProvider } from './app/context/authContext.jsx' //responsavel por gerenciar o estado de autenticacao da aplicacao
+import { BrowserRouter } from 'react-router-dom' //habilita o roteamento e permite a navegacao entre diferentes paginas da aplicacao
+//cria uma raiz react dentro do elemento html com o id 'root'
+createRoot(document.getElementById('root'))
+//renderiza o componente dentro da raiz
+.render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
@@ -14,3 +16,9 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>
 );
 
+/*
+<StrictMode>: Envolve toda a aplicacao para ativar o modo estrito, como mencionado anteriormente.
+<BrowserRouter>: Envolve a aplicacao para habilitar o roteamento.
+<AuthProvider>: Envolve o componente App para fornecer o contexto de autenticacao para todos os componentes filhos.
+<App />: Renderiza o componente principal da aplicacao, que contem toda a logica e estrutura da sua interface do usuario.
+*/
