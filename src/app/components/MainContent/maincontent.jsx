122ce81e800935862613e772b3d357a1671a32b2
BrunaCardoso7 <obitadrawing@gmail.com>
2025-02-04 00:58:56 -0300

    corrige e refatora componente de calendario

--- src/app/components/MainContent/maincontent.jsx
+++ src/app/components/MainContent/maincontent.jsx
@@ -8,6 +8,7 @@ import { ProfileUser } from "../ProfileUser/profileuser";
 import { StartupItem } from "../StartupItem/StartupItem";
 import { Temperatura } from "../Temperatura/Temperatura";
 import { InstagramPosts } from "../InstagramPost/InstagramPost";
+import Calendario from "../Caledario/Calendario";
 
 const MainContent = () => {
     const { user, token, enterprise } = useContext(AuthContext)
@@ -34,69 +35,7 @@ const MainContent = () => {
                         <Temperatura />
                         <ProfileUser />
                         <div className="">
-                            <div className="card w_calender">
-                                <div className="date l-slategray">
-                                    <span>Sunday, December 28</span>
-                                    <span>2017</span>
-                                </div>
-                                <div className="body days">
-                                    <ul className="m-t-10">
-                                        <li>MON</li>
-                                        <li>TUE</li>
-                                        <li>WED</li>
-                                        <li>THU</li>
-                                        <li>FRI</li>
-                                        <li>SAT</li>
-                                        <li>SUN</li>
-                                    </ul>
-                                    <ul>
-                                        <li>1</li>
-                                        <li>2</li>
-                                        <li>3</li>
-                                        <li>4</li>
-                                        <li>5</li>
-                                        <li>6</li>
-                                        <li>7</li>
-                                    </ul>
-                                    <ul>
-                                        <li>8</li>
-                                        <li>9</li>
-                                        <li>10</li>
-                                        <li>11</li>
-                                        <li>12</li>
-                                        <li>13</li>
-                                        <li>14</li>
-                                    </ul>
-                                    <ul>
-                                        <li>15</li>
-                                        <li>16</li>
-                                        <li>17</li>
-                                        <li>18</li>
-                                        <li>19</li>
-                                        <li>20</li>
-                                        <li>21</li>
-                                    </ul>
-                                    <ul>
-                                        <li>22</li>
-                                        <li>23</li>
-                                        <li>24</li>
-                                        <li>25</li>
-                                        <li>26</li>
-                                        <li>27</li>
-                                        <li><em>28</em></li>
-                                    </ul>
-                                    <ul>
-                                        <li>29</li>
-                                        <li>30</li>
-                                        <li>31</li>
-                                        <li>1</li>
-                                        <li>2</li>
-                                        <li>3</li>
-                                        <li>4</li>
-                                    </ul>
-                                </div>
-
-                            </div>
+                            <Calendario/>
                             <InstagramPosts />
                         </div>
                     </div>
@@ -104,9 +43,7 @@ const MainContent = () => {
                     <div class="col-lg-8 col-md-12">
                         <div class="card">
                             <div class="header">
-
                                 <h2><strong>Minhas</strong> Startups</h2>
-
                             </div>
                             <div class="body">
                                 <div class="table-responsive social_media_table">
@@ -122,8 +59,6 @@ const MainContent = () => {
                                             </tr>
                                         </thead>
                                         <tbody>
-
-
                                             {
                                                 enterprise && Object.values(enterprise).map(enter => {
                                                     console.log("aa",enter)
@@ -132,15 +67,12 @@ const MainContent = () => {
                                                     )
                                                 })
                                             }
-
-
                                         </tbody>
                                     </table>
                                 </div>
                             </div>
                         </div>
                         <CardMain />
-
                         {/* timeline */}
                         <div role="tabpanel" className="">
                             <ul className="cbp_tmtimeline">
